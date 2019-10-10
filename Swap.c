afa1aaee4306ddfb4868480fb87db9193d589b51
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-10-10 19:54:18 +0530

    Add files via upload

---
+++ Swap.c
@@ -0,0 +1,44 @@
+/* Accept two characters from user.Swap them if both the characters are small letters
+	 or both the characters are capital letter. In other cases keep the contents as it is.
+	 Input:	a   b	   A  M 	 m   B    1   %	
+	 Output:	b  a	   M  A	 m   B    1   %									*/
+	 
+#include<stdio.h>
+
+void Swap(char *p, char *q)
+{
+	char ch;
+	
+	if(((*p>='A')&&(*p<='Z'))&&((*q>='A')&&(*q<='Z')))
+	{
+		ch=*p;
+		*p=*q;
+		*q=ch;
+	}
+	
+	else if(((*p>='a')&&(*p<='z'))&&((*q>='a')&&(*q<='z')))
+	{
+		ch=*p;
+		*p=*q;
+		*q=ch;
+	}
+} 
+	 
+int main()
+{
+	char ch1, ch2;
+	
+	printf("Enter 1st character: ");
+	scanf("%s",&ch1);
+	 
+ 	printf("Enter 2nd character: ");
+	scanf("%s",&ch2);
+	
+	printf("Before swapping: %c\t%c\n",ch1,ch2);
+	
+	Swap(&ch1, &ch2);
+	
+	printf("After swapping:  %c\t%c\n",ch1,ch2);
+	
+	return 0;
+}
