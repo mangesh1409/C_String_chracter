349d79f375f5ad2c1be9fe9c666e3fd9c7a0045d
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-10-10 13:33:59 +0530

    Add files via upload

---
+++ SCountChar.c
@@ -0,0 +1,42 @@
+/* Accept string & one character from user and return frequency of that charcter in that string
+	Input:	MarvellLlouS Multi OS
+				l
+	Output:	4																			*/
+	
+#include<stdio.h>
+
+int Frequency(char *p, char ch)
+{
+	int i=0;
+	
+	while(*p!='\0')
+	{
+		if(*p==ch)
+		{
+			i++;
+		}
+		p++;
+	}
+	return i;
+}
+
+int main()
+{
+	char Arr[30], ch='\0';
+	
+	int iRet=0;
+	
+	printf("Enter string:");
+	scanf("%[^'\n']s",Arr);
+	
+	printf("Enter character: ");
+	scanf("%s",&ch);
+	
+	iRet=Frequency(Arr,ch);
+	
+	printf("Frequency of %c is %d",ch,iRet);
+	
+	printf("\n");
+
+	return 0;
+}
