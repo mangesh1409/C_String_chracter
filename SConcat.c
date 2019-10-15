d693660050f3c63893da7f85798430bd9fd56d56
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-10-16 00:24:21 +0530

    Add files via upload

---
+++ SConcat.c
@@ -0,0 +1,55 @@
+/* Accept  2 strings from user and concat second string
+	after first string.
+	Input :    String 1- Marvellous Infosystems
+				String 2- Logic Building	
+
+	Output : Marvellous Infosystems Logic Building					*/
+	
+#include<stdio.h>
+
+void SConcat(char * p, char * q)
+{
+	if((p==NULL)||(q==NULL))
+	{
+		return;
+	}
+	
+	while(*p!='\0')
+	{
+		p++;
+	}
+	
+	*p=' ';
+	  p++;
+	 
+	while(*q!='\0')
+	{
+		*p=*q;
+		  q++;
+		  p++;
+	}
+	
+	*q='\0';
+}
+
+int main()
+{
+	char arr[50];
+	char brr[30];
+	
+	//char arr[50] = "Marvellous Infosystems";
+	//char brr[30] = "Logic Building";
+	
+	printf("Enter 1st string: ");
+	scanf("%[^'\n']s",arr);
+	
+	printf("Enter 2nd string: ");
+	scanf(" %[^'\n']s",brr);
+	
+	SConcat(arr,brr);
+	
+	printf("Final string:  %s ",arr);
+	printf("\n");
+	
+	return 0;
+}	
